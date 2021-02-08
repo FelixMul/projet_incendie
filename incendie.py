07a6d1b891994946a90c149bd7a2bda4b7c28d47
FelixMul <71290826+FelixMul@users.noreply.github.com>
2021-02-08 15:01:14 +0100

    Create incendie.py

---
+++ incendie.py
@@ -0,0 +1,8 @@
+#########################################
+# groupe DLMP
+# Felix Muller  
+# James Burat
+# Lucas FEDERSPIEL
+# Bixente Bengochea
+# https://github.com/FelixMul/projet_incendie
+#########################################
