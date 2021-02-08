2ddee41892f4994d1bc3c3f658eb55dbdab8741a
Felix.MULLER <felix.muller@ens.uvsq.fr>
2021-02-08 15:16:08 +0100

    premiers pas

--- incendie.py
+++ incendie.py
@@ -6,3 +6,20 @@
 # Bixente Bengochea
 # https://github.com/FelixMul/projet_incendie
 #########################################
+
+#import des librairies
+
+
+
+# Définition des constantes 
+DUREE_FEU = 
+
+
+# Définition des variables globales
+
+
+# Définition des fonctions
+
+
+# Programme principal contenant la définition des widgets et des 
+# événements qui leur sont liés et l’appel à la boule de gestion des événements
