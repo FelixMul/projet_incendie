c9efe6fe406d10760a6862848193291816a11fbd
Overlord <bixente.bengochea@ens.uvsq.fr>
2021-02-08 16:57:05 +0100

    création interface graphique

--- incendie.py
+++ incendie.py
@@ -9,14 +9,14 @@
 
 #import des librairies
 import tkinter as tk
-import random
+import random as rd
 
 
 # Définition des constantes 
-DUREE_FEU = 
-DUREE_CENDRE =
-LARGEUER = 
-HAUTEUR =
+DUREE_FEU = 0
+DUREE_CENDRE = 0
+LARGEUER = 600
+HAUTEUR = 400
 COULEURS = ["blue", "red", "green", "yellow", "black", "gray50"]
 
 # Définition des variables globales
@@ -24,28 +24,31 @@ COULEURS = ["blue", "red", "green", "yellow", "black", "gray50"]
 
 # Définition des fonctions
 def nouvelle_etape():
-    for i in range(HAUTEUR):
-        for j in range(LARGEUER):
-            if 
+    pass
 
 
 # Programme principal contenant la définition des widgets et des 
 # événements qui leur sont liés et l’appel à la boule de gestion des événements
-
-Bouton_terrain = tk.Button(racine, command=pass)
-Bouton_save = tk.Button(racine, command=pass)
-Bouton_load = tk.Button(racine, command=pass)
-Bouton_step = tk.Button(racine, command=pass)
-Bouton_start = tk.Button(racine, command=pass)
-Bouton_stop = tk.Butoon(racine, command=pass)
-Bouton_speedup = tk.Butoon(racine, command=pass)
-Bouton_speeddown = tk.Butoon(racine, command=pass)
+racine = tk.Tk()
+racine.title("Propagation d'un incendie")
+terrain = tk.Canvas(racine, width=600, height=400, bg="black")
+Bouton_terrain = tk.Button(racine, text="Terrain")
+Bouton_save = tk.Button(racine, text="Save")
+Bouton_load = tk.Button(racine, text="Load")
+Bouton_step = tk.Button(racine, text="Step")
+Bouton_start = tk.Button(racine, text="Start")
+Bouton_stop = tk.Button(racine, text="Stop")
+Bouton_speedup = tk.Button(racine, text="Speedup")
+Bouton_speeddown = tk.Button(racine, text="Slowdown")
 
 Bouton_terrain.grid(row=0, column=0)
-Bouton_save.grid(row=0, column=0)
-Bouton_load.grid(row=0, column=0)
-Bouton_step.grid(row=0, column=0)
-Bouton_start.grid(row=0, column=0)
-Bouton_stop.grid(row=0, column=0)
-Bouton_speedup.grid(row=0, column=0)
-Bouton_speeddown.grid(row=0, column=0)
+Bouton_save.grid(row=0, column=1)
+Bouton_load.grid(row=0, column=2)
+Bouton_step.grid(row=0, column=3)
+Bouton_start.grid(row=0, column=4)
+Bouton_stop.grid(row=1, column=4)
+Bouton_speedup.grid(row=2, column=4)
+Bouton_speeddown.grid(row=3, column=4)
+terrain.grid(row=1, column=0, columnspan=4, rowspan=3)
+
+racine.mainloop()
