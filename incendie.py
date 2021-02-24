244eb562766b6548c0a8e3a6d95b02c2cd7cabf9
Felix.MULLER <felix.muller@ens.uvsq.fr>
2021-02-24 14:40:49 +0100

    Fin de ma partie (Felix). La touche "espace" porte la meme fonction que "step". Cells[n][3] = DUREE_FEU. Cells[n][4] = DUREE_CENDRE

--- incendie.py
+++ incendie.py
@@ -10,52 +10,83 @@
 #import des librairies
 import tkinter as tk
 import random as rd
+import copy as cp
 
 
-# Définition des constantes 
-DUREE_FEU = 0
-DUREE_CENDRE = 0
+# Définition des constantes
+DUREE_FEU = 10
+DUREE_CENDRE = 10
 LARGEUR = 600
 HAUTEUR = 400
 COULEURS = ["blue", "green", "yellow"]
-TAILLE_CARRE = 20
+COTE = 20 #Taille du cote, doit etre un diviseur commun de la largeur  et de la hauteur
 
 # Définition des variables globales
 Cells = []
 
 ### Cells est la liste qui contient les coordonées et la couleur de chaque cellules sous la forme [x, y, "color"]
+### Cells_step est la liste qu'on utilise pour passer a la prochaine etape
 
-###  Définition des fonctions
-
-def nouvelle_etape():
+# Définition des fonctions
+def nouvelle_etape(event = None):
     # Fonction permettant de passer a une nouvelle etape
     global Cells
-    Cells_step = Cells
-    for n in Cells:#les parcelles de bords ne peuvent pas prendre feu
-        if n[0] != 1 and n[1] != 1 and n[0] != LARGEUR + 1 - COTE and n[1] != HAUTEUR + 1 - COTE:
-            if n[2] == "green":
+    Cells_step = cp.deepcopy(Cells)
+    for n in range(len(Cells)):#les carres de bords ne peuvent pas prendre feu
+        if n > (HAUTEUR / COTE) and n < ((HAUTEUR // COTE) * (LARGEUR // COTE) - HAUTEUR // COTE) and n % (HAUTEUR / COTE) != 0 and (n + 1) % (HAUTEUR / COTE) != 0: 
+            if Cells[n][2] == "green":
                 b = 0
-                for i in range(n[0] - COTE, n[0] + COTE + 1, COTE):
-                    for j in range(n[1] - COTE, n[1] + COTE + 1, COTE):
-                        if (i // 20) * (j // 20) < (HAUTEUR - 1) * (LARGEUR - 1) / (COTE ** 2) - 1:
-                            if Cells[int((i // COTE) * HAUTEUR / COTE + (j // COTE))][2] == "red":
-                                b += 0.1
-                #on regarde la couleur de chaque parcelle au tour de parcelle n
+                for m in range(n - 1 - int(HAUTEUR / COTE), n + 2 - int(HAUTEUR / COTE)):
+                    if Cells[m][2] == "red":
+                        b += 0.1
+                for m in range(n - 1 + int(HAUTEUR / COTE), n + 2 + int(HAUTEUR / COTE)):
+                    if m < len(Cells):
+                        if Cells[m][2] == "red":
+                            b += 0.1
+                if Cells[n - 1][2] == "red":
+                    b += 0.1
+                if Cells[n + 1][2] == "red":#on regarde la couleur de chaque parcelle au tour de Cells[n]
+                    b += 0.1
                 a = rd.random()
                 if a < b:
-                    #une parcelle de forêt prend feu avec la probabilité 0.1 × nf
-                    Cells_step[int(n[0] // COTE * HAUTEUR / COTE + n[1] // COTE)][2] = "red"
-                terrain.create_rectangle(n[0], n[1], n[0]+COTE, n[1]+COTE, fill = n[2])
-            elif n[2] == "yellow":
-                for i in range(n[0] - COTE, n[0] + COTE + 1, COTE):
-                    for j in range(n[1] - COTE, n[1] + COTE + 1, COTE):
-                        if (i // 20) * (j // 20) < (HAUTEUR - 1) * (LARGEUR - 1) / (COTE ** 2) - 1:
-                            if Cells[int((i // COTE) * HAUTEUR / COTE + (j // COTE))][2] == "red":
-                                Cells_step[int(n[0] // COTE * HAUTEUR / COTE + n[1] // COTE)][2] = "red"
-                terrain.create_rectangle(n[0], n[1], n[0]+COTE, n[1]+COTE, fill=Cells_step[int(n[0] // COTE * HAUTEUR / COTE + n[1] // COTE)][2])
-    Cells = Cells_step
-
-
+                    #une parcelle de forêt prend feu avec la probabilité 0.1 × nf 
+                    Cells_step[n][2] = "red"
+                    Cells_step[n][3] = DUREE_FEU
+                terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill = Cells_step[n][2])
+            elif Cells[n][2] == "yellow":
+                for m in range(n - 1 - int(HAUTEUR / COTE), n + 2 - int(HAUTEUR / COTE)):
+                    if Cells[m][2] == "red":
+                        Cells_step[n][2] = "red"
+                        Cells_step[n][3] = DUREE_FEU
+                for m in range(n - 1 + int(HAUTEUR / COTE), n + 2 + int(HAUTEUR / COTE)):
+                    if m < len(Cells):
+                        if Cells[m][2] == "red":
+                            Cells_step[n][2] = "red"
+                            Cells_step[n][3] = DUREE_FEU
+                if Cells[n - 1][2] == "red":
+                    Cells_step[n][2] = "red"
+                    Cells_step[n][3] = DUREE_FEU
+                if Cells[n + 1][2] == "red":
+                    Cells_step[n][2] = "red"
+                    Cells_step[n][3] = DUREE_FEU
+                terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill=Cells_step[n][2])
+    #fonction permettant de regarder la valeur DUREE_FEU
+    for n in Cells_step:
+        if n[2] == "red":
+            n[3] -= 1
+            if n[3] == 0:
+                n[2] = "gray"
+                n[4] = DUREE_CENDRE
+                terrain.create_rectangle(n[0], n[1], n[0]+COTE, n[1]+COTE, fill=n[2])
+    #fonction permettant de regarder la valeur DUREE_CENDRE
+    for n in Cells_step:
+        if n[2] == "gray":
+            n[4] -= 1
+            if n[4] == 0:
+                n[2] = "black"
+                terrain.create_rectangle(n[0], n[1], n[0]+COTE, n[1]+COTE, fill=n[2])
+            
+    Cells = cp.deepcopy(Cells_step)
 
 def clic_feu(event):
     """Fonction permettant de créer une cellule de feu à l'endroit ou la souris se situe avec un clic gauche"""
@@ -64,35 +95,35 @@ def clic_feu(event):
     y = event.y
 
     for n in range(len(Cells)):
-        if (Cells[n][0] + TAILLE_CARRE - 1 >= x >= Cells[n][0] and
-        Cells[n][1] + TAILLE_CARRE - 1 >= y >= Cells[n][1] and
+        if (Cells[n][0] + COTE - 1 >= x >= Cells[n][0] and
+        Cells[n][1] + COTE - 1 >= y >= Cells[n][1] and
         Cells[n][2] != "blue"):
-            terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+TAILLE_CARRE, Cells[n][1]+TAILLE_CARRE, fill="red")
+            terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="red")
             Cells[n][2] = "red"
+            Cells[n][3] = DUREE_FEU
 
 
 def gen_terrain():
-    """Fonction permettant de générer le terrain. La liste Cells contient les coordonnées de chaques cellules ainsi que leurs couleurs respective"""
+    """Fonction permettant de générer le terrain. La liste Cells contient les coordonnées de chaques cellules ainsi que leurs couleurs respective, leur DUREE_FEU et DUREE_CENDRE"""
 
     global Cells
     Cells = []
     terrain.delete("all")
 
-    for i in range(1, LARGEUR, TAILLE_CARRE):
-        for j in range(1, HAUTEUR, TAILLE_CARRE):
-            Cells.append([i, j, rd.choice(COULEURS)])
-    
+    for i in range(1, LARGEUR, COTE):
+        for j in range(1, HAUTEUR, COTE):
+            Cells.append([i, j, rd.choice(COULEURS), 0, 0])
     for n in range(len(Cells)):
 
         if Cells[n][2] == "blue":
-             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+TAILLE_CARRE, Cells[n][1]+TAILLE_CARRE, fill="blue")
+             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="blue")
         elif Cells[n][2] == "green":
-             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+TAILLE_CARRE, Cells[n][1]+TAILLE_CARRE, fill="green")
+             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="green")
         elif Cells[n][2] == "yellow":
-             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+TAILLE_CARRE, Cells[n][1]+TAILLE_CARRE, fill="yellow")
+             terrain.create_rectangle(Cells[n][0], Cells[n][1], Cells[n][0]+COTE, Cells[n][1]+COTE, fill="yellow")
 
 
-# Programme principal contenant la définition des widgets et des 
+# Programme principal contenant la définition des widgets et des
 # événements qui leur sont liés et l’appel à la boule de gestion des événements
 
 racine = tk.Tk()
@@ -108,6 +139,7 @@ Bouton_speedup = tk.Button(racine, text="Speedup")
 Bouton_speeddown = tk.Button(racine, text="Slowdown")
 
 terrain.bind('<Button-1>', clic_feu)
+racine.bind('<space>', nouvelle_etape)
 
 
 Bouton_terrain.grid(row=0, column=0)
